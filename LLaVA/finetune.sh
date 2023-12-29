376da5e4823a513fbc51b89f18a28f7c5e77724b
Yue Zhao <2826190541@qq.com>
2023-12-29 21:28:18 +0800

    clean up llava

--- LLaVA/finetune.sh
+++ LLaVA/finetune.sh
@@ -3,8 +3,8 @@
 # Uncomment and set the following variables correspondingly to run this script:
 
 ################## VICUNA ##################
-# PROMPT_VERSION=v1
-# MODEL_VERSION="vicuna-v1-3-7b"
+PROMPT_VERSION=v1
+MODEL_VERSION="vicuna-13b"
 ################## VICUNA ##################
 
 ################## LLaMA-2 ##################
@@ -14,19 +14,17 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --lora_enable True \
-    --bits 4 \
-    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --model_name_or_path ./checkpoint/$MODEL_VERSION \
     --version $PROMPT_VERSION \
-    --data_path ./playground/data/llava_instruct_80k.json \
-    --image_folder /path/to/coco/train2017 \
-    --vision_tower openai/clip-vit-large-patch14 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+    --data_path ./data/llava_instruct_80k.json \
+    --image_folder ./data/pretrain/train2017 \
+    --vision_tower /data/CLIP/clip-vit-large-patch14 \
+    --pretrain_mm_mlp_adapter ./checkpoint/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune_lora \
+    --output_dir ./checkpoint/llava-$MODEL_VERSION-finetune \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -43,6 +41,6 @@ deepspeed llava/train/train_mem.py \
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --lazy_preprocess True \
     --dataloader_num_workers 4 \
-    --report_to wandb
+    --lazy_preprocess True \
+    # --report_to wandb
