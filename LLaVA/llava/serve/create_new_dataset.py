c7a9ab7dc7b77dc4a73ae097dd0ed093d387b748
Yue Zhao <2826190541@qq.com>
2023-12-29 17:44:16 +0800

    fix llava generation code

--- LLaVA/llava/serve/create_new_dataset.py
+++ LLaVA/llava/serve/create_new_dataset.py
@@ -18,12 +18,13 @@ from transformers import TextStreamer
 from tqdm import tqdm
 
 dataset = 'deepfashion'
-karpathy_json_path=f'../../data/{dataset}/dataset_{dataset}.json'
-image_folder=f'../../data/{dataset}/images'
+karpathy_json_path=f'/data/CourseProject/data/{dataset}/dataset_{dataset}.json'
+image_folder=f'/data/CourseProject/data/{dataset}/images'
 
 with open(karpathy_json_path, 'r') as j:
     data = json.load(j)
-
+print(data.keys())
+data['images'] = data['images'][:2000]
 def load_image(image_file):
     if image_file.startswith('http') or image_file.startswith('https'):
         response = requests.get(image_file)
@@ -54,21 +55,27 @@ def main(args):
     else:
         args.conv_mode = conv_mode
 
-    inp = 'Please describe this image in deepfashion dataset style.'
+    
     
     for img in tqdm(data['images']):
+        inp = 'Please describe this image in deepfashion dataset style.'
         path = os.path.join(image_folder, img['filename'])
         conv = conv_templates[args.conv_mode].copy()
 
         image = load_image(path)
         image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().cuda()
-
-        if model.config.mm_use_im_start_end:
-            inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+        # print(f"Image: {path}")
+        if image is not None:
+            # first message
+            if model.config.mm_use_im_start_end:
+                inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+            else:
+                inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
+            conv.append_message(conv.roles[0], inp)
+            image = None
         else:
-            inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
-        conv.append_message(conv.roles[0], inp)
-        image = None
+            # later messages
+            conv.append_message(conv.roles[0], inp)
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
 
@@ -76,7 +83,8 @@ def main(args):
         stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
         keywords = [stop_str]
         stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
-        streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+        # streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+        streamer = None
 
         with torch.inference_mode():
             output_ids = model.generate(
@@ -84,7 +92,7 @@ def main(args):
                 images=image_tensor,
                 do_sample=True,
                 temperature=0.2,
-                max_new_tokens=1024,
+                max_new_tokens=200,
                 streamer=streamer,
                 use_cache=True,
                 stopping_criteria=[stopping_criteria])
@@ -111,5 +119,5 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
     main(args)
-    with open(f'../../data/{dataset}/new_dataset_{dataset}.json', 'w') as fw:
+    with open(f'/data/CourseProject/data/{dataset}/precise_dataset_{dataset}.json', 'w') as fw:
         json.dump(data, fw)
